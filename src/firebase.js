7bfaf8e22c2ae32cb124f30a7137295e395060a8
Sandesh-Sinha <sandesh85210@gmail.com>
2020-10-16 23:53:08 +0530

    Header Component

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyB6H1S5DkY-diAPQhsamGFLSMMrUzBI2w4",
+    authDomain: "buyamz-3fe99.firebaseapp.com",
+    databaseURL: "https://buyamz-3fe99.firebaseio.com",
+    projectId: "buyamz-3fe99",
+    storageBucket: "buyamz-3fe99.appspot.com",
+    messagingSenderId: "611077605778",
+    appId: "1:611077605778:web:a8d41c7d769f794bd1594b",
+    measurementId: "G-6H41SW759H"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
